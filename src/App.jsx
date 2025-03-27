73eb2d18a3db9769af04baaf0a4f891f31af95be
aboaasim <awkheralkalem@gmail.com>
2025-03-27 03:19:59 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+<span class="hljs-keyword">import</span> <span class="hljs-title class_">React</span> <span class="hljs-keyword">from</span> <span class="hljs-string">'react'</span>
+
+<span class="hljs-keyword">function</span> <span class="hljs-title function_">App</span>(<span class="hljs-params"></span>) {
+  <span class="hljs-keyword">return</span> (
+    <span class="language-xml"><span class="hljs-tag"><<span class="hljs-name">div</span>></span>
+      <span class="hljs-tag"><<span class="hljs-name">h1</span>></span>أواخر الكلم<span class="hljs-tag"></span>
+    <span class="hljs-tag"></span></span>
+  )
+}
+
+<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> <span class="hljs-title class_">App</span>
